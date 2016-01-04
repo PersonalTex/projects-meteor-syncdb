0c71de473e48014d37759e0454184be82961e490
Paolo tessari <personal.tex@gmail.com>
2016-01-05 00:05:23 +0100

    04.01.2016

--- main.js
+++ main.js
@@ -1,95 +1,57 @@
+//"use strict";
+
 var Future = Npm.require('fibers/future');
 var util = Npm.require('util');
 
 
-if (Meteor.isClient) {
-  // counter starts at 0
-  //Session.setDefault('counter', 0);
+Meteor.startup(function () {
+  if (Meteor.isServer) {
+      //var options = {};
+      var connMgr = null;
+      var connection = null;
+      var op = null;
+      //var dbUtil = null;
 
-  Template.oplog.helpers({
-    /*
-    counter: function () {
-      return Session.get('counter');
-    }*/
-  });
+      var Setup = function () {
 
-  Template.oplog.events({
-    /*
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-    */
-  });
-}
+          console.log("1");
+          connMgr = new DbConnectionManager(Meteor.settings.DbConnections);
+          console.log("2");
 
-  Meteor.startup(function () {
-
-
-      if (Meteor.isServer) {
-          var options = {};
-          /*
-          var commandMgr = null;
-          var docUtil = null;
-          */
-          var connManager = null;
-          var connection = null;
-          var op = null;
-          var dbUtil = null;
-
-          var Setup = function () {
-              if(!openConnection().wait())
-                console.log("openConnection failed")
-              else
-                initOpLog();
-          }
-
-
-          var openConnection = function () {
-              var future = new Future();
-              console.log('openConnection');
-
-
-              try {
-
-                  connMgr = new DbConnectionManager(Meteor.settings.DbConnections);
-
-                  connection = connMgr.open(Meteor.settings.OpLog.Databases.legacy).wait();
-
-
-                  future.return(connection.dbInstance!=null );
-              }
-              catch(e) {
-                  future.return(false);
-              }
-              return future.wait();
-
-              //process.nextTick(initOpLog);
-          }.future();
-
-          var initOpLog = function () {
-
-              //
-              // var future = new Future();
-
-              console.log('initOpLog');
-
-
-
-              var uri = connMgr.getConnectionString(Meteor.settings.OpLog.Databases.local);
-              var filter = util.format('(^%s.doc)', Meteor.settings.DbConnections[Meteor.settings.OpLog.Databases.local].db);
-              //options.commandManager = new OpSequelizeCommandManager(connection, new DbUtil(Meteor.settings.Def));
-              //var dbUtil = new DbUtil(Meteor.settings.Def)
-
-              var dbTables = new DbTables(Meteor.settings.Def.Collections);
-
-              op = new OpLogWrite(uri, filter, connection, dbTables);
-              op.run();
-
-          }
-
-          Setup();
+          console.log(connMgr);
+          if(!openConnection(Meteor.settings.OpLog.Databases.legacy).wait())
+            console.log("openConnection failed")
+          else
+            initOpLog();
       }
+
+
+      var openConnection = function (options) {
+          var future = new Future();
+          console.log('openConnection '+ options);
+          try {
+
+
+              connection = connMgr.open(options).wait();
+              future.return(connection.dbInstance!=null );
+          }
+          catch(e) {
+              future.return(false);
+          }
+          return future.wait();
+      }.future();
+
+      var initOpLog = function () {
+          console.log('initOpLog');
+
+          var uri = connMgr.getConnectionString(Meteor.settings.OpLog.Databases.local);
+          var filter = util.format('(^%s.doc)', Meteor.settings.DbConnections[Meteor.settings.OpLog.Databases.local].db);
+          var dbTables = new DbTables(Meteor.settings.Def.Collections);
+          op = new OpLogWrite(uri, filter, connection, dbTables);
+          op.run();
+      }
+      Setup();
+  }
 });
 
 
